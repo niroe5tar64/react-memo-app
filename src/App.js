3504f036ce696f3acaf607386beef022484b2f9d
niroe5tar64 <niroe5tar64@gmail.com>
2020-10-25 05:46:12 +0000

    Add: persist

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Memo from './memo/Memo';
 import AddForm from './memo/AddForm';
 import FindForm from './memo/FindForm';
 import DelForm from './memo/DelForm';
+import PersistForm from './memo/PersistForm';
 
 // Appコンポーネント
 class App extends Component {
@@ -30,6 +31,9 @@ class App extends Component {
               </td>
               <td style={this.td}>
                 <DelForm />
+              </td>
+              <td style={this.td}>
+                <PersistForm />
               </td>
             </tr>
           </tbody>
